55af451bb01c17ac800a3b98324cfa2bb45615c4
A.Kosorotikov <darkringfire@gmail.com>
2022-03-31 20:52:55 +0800

    Add polls as installed app

--- bgb_manager/settings.py
+++ bgb_manager/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'addr',
+    'addr.apps.AddrConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
