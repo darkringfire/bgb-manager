bc6fa80408a091a50df3b7a6e3cf5791ab48b80c
A.Kosorotikov <darkringfire@gmail.com>
2022-04-01 18:21:03 +0800

    Add vote functionality

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from sorcery import dict_of
 
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 def index(request):
@@ -23,4 +24,22 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            'polls/detail.html',
+            dict_of(question, error_message="You didn't select a choice.")
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question.id,))
+        )
