51fb7d0b0d451f55b88ea56e3d7484d8e0c7e80e
A.Kosorotikov <darkringfire@gmail.com>
2022-04-01 16:30:36 +0800

    Add detail template to polls

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from sorcery import dict_of
+
 
 from polls.models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = dict_of(latest_question_list)
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', dict_of(question))
 
 
 def results(request, question_id):
