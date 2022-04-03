e42724ff338bcd13a5e06f5941ff4bf4a39bdc91
A.Kosorotikov <darkringfire@gmail.com>
2022-04-03 23:24:01 +0800

    Fix question list
    Do not show question from future

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from sorcery import dict_of
+from django.utils import timezone
 
 from polls.models import Choice, Question
 
@@ -12,7 +13,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
