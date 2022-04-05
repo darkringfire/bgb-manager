30e9a485d9a2fbaea6e6a0470e30c6d8e0dc5d30
A.Kosorotikov <darkringfire@gmail.com>
2022-04-05 17:00:28 +0800

    Test requests

--- kladr/models.py
+++ kladr/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+region_id = '3800000000000'
+
+class KladrManager(models.Manager):
+    def get_queryset(self):
+        return models.QuerySet()
+
+
+class Region(models.Model):
+    id = models.CharField(max_length=13, primary_key=True)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+
+    def __str__(self):
+        return self.name
+
+
